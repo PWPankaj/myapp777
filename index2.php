d181a88f522109cf149328c2edcaa526c0890284
AIC Digital <53875525+PWPankaj@users.noreply.github.com>
2023-04-01 18:21:37 +0530

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,39 @@
+<html>
+<body>
+<?php
+
+echo "printing on browser!!";
+
+?>
+
+<p>
+this is a paraghraph
+</p>
+<p>
+this is <b>second</b> <u>paragraph</u>
+
+</p>
+
+<p>
+
+
+this is para with list
+
+<ul>
+<li>
+tea
+</li>
+<li>
+coffee
+</li>
+
+<li>
+milk
+</li>
+
+</ul>
+
+</p>
+</body>
+</html>
+
